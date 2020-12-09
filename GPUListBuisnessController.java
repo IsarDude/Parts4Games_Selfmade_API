392aa617db8ca5531a2af2b0c3e38bd954d74ef1
rebibabi <75579936+rebibabi@users.noreply.github.com>
2020-12-09 08:25:01 +0100

    Add files via upload

---
+++ GPUListBuisnessController.java
@@ -0,0 +1,12 @@
+package business;
+
+import java.util.List;
+
+public class GPUListBuisnessController {
+	
+	public List<String> getGPUList(int frequency, int memory, String company, String model, String generation, float price){
+		//Get-request to Ebay API: Get all gpus's with specified requirements as JSON
+		//Convert JSON to List and return
+		return null;
+	}
+}
