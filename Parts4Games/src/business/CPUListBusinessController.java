cf49920c197c69c04607aefd51efdc62306823e1
rebibabi <student@rest-vmwarevirtualplatform>
2020-12-09 09:56:47 +0100

    "business: CPUListBusinessController 		   GPUListBusinessController  model:    Motherboard  		   CPU  service:  CpuRessource  		   CpuListRessource  		   GpuListRessource  "

---
+++ Parts4Games/src/business/CPUListBusinessController.java
@@ -0,0 +1,12 @@
+package business;
+
+import java.util.List;
+
+public class CPUListBusinessController {
+
+	public List<String> getCPUList(int socket, int frequency, int cores, String company, String model, int generation, float price){
+		//Get-request to Ebay API: Get all cpu's with specified requirements as JSON
+		//Convert JSON to List and return
+		return null;
+	}
+}
