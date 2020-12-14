458a0b42a2c510ac28019b356aa8b0490495c833
rebibabi <student@rest-vmwarevirtualplatform>
2020-12-14 23:10:06 +0100

    "add GPUListBusinessController.java"

---
+++ Parts4Games/src/data/CPU.java
@@ -0,0 +1,104 @@
+package data;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement //Aktiviere JSON/XML-Konvertierung 
+public class CPU {
+	private int frequency;
+	private int socket;
+	private String company;
+	private String model;
+	private int generation;
+	private int cores;
+	private float price;
+	
+	public CPU(int aSocket, int aFrequency, int aCores, String aCompany, String aModel, int aGeneration, float aPrice) {
+		super();
+		this.frequency= aFrequency;
+		this.cores = aCores;
+		this.company = aCompany;
+		this.model = aModel;
+		this.generation= aGeneration;
+		this.socket = aSocket;
+		this.price=aPrice;
+		
+	}
+	
+	@XmlElement(name="socket")
+	public int getSocket() {
+		return socket;
+	}
+
+	public void setSocket(int socket) {
+		this.socket = socket;
+	}
+	
+	@XmlElement(name="frequency")
+	public int getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(int frequency) {
+		this.frequency = frequency;
+	}
+	
+	@XmlElement(name="cores")
+	public int getCores() {
+		return cores;
+	}
+
+	public void setCores(int cores) {
+		this.cores = cores;
+	}
+
+	@XmlElement(name="company")
+	public String getCompany() {
+		return company;
+	}
+	
+	
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	
+	@XmlElement(name="model")
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	@XmlElement(name="generation")
+	public int getGeneration() {
+		return generation;
+	}
+
+	public void setGeneration(int generation) {
+		this.generation = generation;
+	}
+	
+	@XmlElement(name="price")
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	public String getKeywordString() {
+		String keywords = "CPU "+ frequency + "Hz ";
+		if(company != null) {
+			keywords = keywords +" " + company;
+		}
+		if(model != null) {
+			keywords = keywords+ " " + model;
+		}
+		return keywords;
+	}
+	
+	
+}
---
+++ Parts4Games/src/rest/CpuListService.java
@@ -0,0 +1,35 @@
+package rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import business.CpuListBusinessController;
+
+@Path("/cpuList")
+public class CpuListService {
+
+	@Inject
+	private CpuListBusinessController CpuListBusinessController;
+	
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getCPUList(int socket, int frequency, int cores, String company, String model, int generation, float price) {
+		try {
+			return Response.ok(CpuListBusinessController.getCPUList(socket, frequency, cores, company, model, generation, price)).build();
+		}catch(Exception e) {
+			e.printStackTrace();
+			return Response.status(503)
+					.type(MediaType.TEXT_PLAIN)
+					.entity(e.getMessage())
+					.build();
+		}
+	}
+}
+	
+
